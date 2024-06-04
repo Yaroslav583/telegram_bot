3c5ccc812e31ea72298f17d3c2f4fe5076544e58
Yaroslav <makhinko180601@gmail.com>
2024-06-04 15:20:28 +0200

    fixed

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,23 @@
 const functions = require("firebase-functions");
 const {Telegraf} = require("telegraf");
 
-
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-
 const TELEGRAM_TOKEN = functions.config().telegram.token;
 
-// Инициализация Telegraf с токеном
 const bot = new Telegraf(TELEGRAM_TOKEN);
 
-bot.start((ctx) => {
+bot.command("start", (ctx) => {
   ctx.reply("t.me/yarik58_bot/tap");
 });
 
-
-exports.telegramWebhook = functions.https.onRequest((req, res) => {
-  bot.handleUpdate(req.body, res);
+exports.telegramWebhook = functions.https.onRequest(async (req, res) => {
+  try {
+    await bot.handleUpdate(req.body);
+    res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 });
